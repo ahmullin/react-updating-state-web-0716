e672dce941fd5297dd7c640905f613be8666ca6b
Alison <alisonhmullin@gmail.com>
2016-09-26 14:06:46 -0400

    complete updating state code along

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const app = "I don't do much.";
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      hasBeenClicked: false,
+      currentTheme: 'blue',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      hasBeenClicked: true,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.hasBeenClicked ? 'not' : null} been clicked yet!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+}
